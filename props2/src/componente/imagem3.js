2cad9bc1edc5344b3840db0e27b0c3b0d8b2071c
vai na web <vainawebcpx@gmail.com>
2019-12-11 12:13:46 -0300

    aula de props2

---
+++ props2/src/componente/imagem3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Imagem(){
+    return (
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1O09_A8Nx-UdGJTQpbNV5qcw09pekLYyDcV_br7sKJxOLmvGR&s'></img>
+    )
+}
